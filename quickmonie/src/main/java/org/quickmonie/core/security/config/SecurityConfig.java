00ae902c4161c0c059a868352631b97bad5d3bfb
Theophilus Danladi Umar <theophilusumar27@gmail.com>
2025-11-05 01:22:16 +0100

    Add DemoController with a basic hello endpoint

---
+++ quickmonie/src/main/java/org/quickmonie/core/security/config/SecurityConfig.java
@@ -0,0 +1,18 @@
+package org.quickmonie.core.security.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http)throws Exception {
+        return http
+                .authorizeHttpRequests(c->c.anyRequest().permitAll())
+                .build();
+    }
+
+}
