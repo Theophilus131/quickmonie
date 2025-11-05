978d1eadd77732a38d03babdb0cf6122d2dc0142
Theophilus Danladi Umar <theophilusumar27@gmail.com>
2025-11-05 01:23:53 +0100

    Add QuickmonieApplicationTests with contextLoads test

---
+++ quickmonie/src/main/java/org/quickmonie/core/QuickmonieApplication.java
@@ -0,0 +1,14 @@
+package org.quickmonie.core;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuickmonieApplication {
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(QuickmonieApplication.class, args);
+	}
+
+}
