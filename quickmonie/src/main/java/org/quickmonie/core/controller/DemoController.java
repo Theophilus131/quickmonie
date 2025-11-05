7cda813f0969e0fb2b6b3a5417fa0c34f371dcdf
Theophilus Danladi Umar <theophilusumar27@gmail.com>
2025-11-05 01:22:09 +0100

    Add DemoController with a basic hello endpoint

---
+++ quickmonie/src/main/java/org/quickmonie/core/controller/DemoController.java
@@ -0,0 +1,13 @@
+package org.quickmonie.core.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class DemoController {
+
+    @GetMapping
+    public String hello() {
+        return "Hello World!";
+    }
+}
