9dc81b0e7673ec7999ff27d4a86e39bedd88b507
Theophilus Danladi Umar <theophilusumar27@gmail.com>
2025-11-05 01:23:42 +0100

    Add QuickmonieApplicationTests with contextLoads test

---
+++ quickmonie/src/test/java/org/quickmonie/core/QuickmonieApplicationTests.java
@@ -0,0 +1,13 @@
+package org.quickmonie.core;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QuickmonieApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
